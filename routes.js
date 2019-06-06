106844741695a5fbb51032e5ccfce79381684b68
Ben Sherwood <48200039+ben-sherwood@users.noreply.github.com>
2019-06-07 11:49:27 +1200

    merging dev to ben

--- routes.js
+++ routes.js
@@ -1,137 +1,78 @@
-<<<<<<< HEAD
-const express = require('express')
-const router = express.Router()
-const data = require('./data')
-// const fs = require("fs")
-=======
 const express = require("express");
 const router = express.Router();
 const data = require("./data");
 const fs = require("fs");
->>>>>>> 34ff2ffcac38b0b617a6c99b8c63c161d94b5629
 
 const viewData = {
     data: data
 };
 
 //index route
-<<<<<<< HEAD
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     // res.send('is it working?')
-    res.redirect('/profiles') // update to redirect to profiles
-})
+    res.redirect("/profiles"); // update to redirect to profiles
+});
 
-router.get('/profiles', (req, res) => {
+router.get("/profiles", (req, res) => {
     const viewData = {
         data: data
-    }
-    res.render('home', viewData.data)
-=======
-router.get("/", (req, res) => {
-  // res.send('is it working?')
-  res.redirect("/profiles"); // update to redirect to profiles
+    };
+    console.log(viewData.data);
+    res.render("home", viewData.data);
 });
->>>>>>> 34ff2ffcac38b0b617a6c99b8c63c161d94b5629
 
-    router.get("/profiles", (req, res) => {
-        const viewData = {
-            data: data
-        };
-        console.log(viewData.data);
-        res.render("home", viewData.data);
+router.get("/profiles/:id", (req, res) => {
+    const viewData = {
+        data: data
+    };
+    //console.log(viewData.data);
+
+    const profile = viewData.data.profiles.find(item => {
+        return item.id == req.params.id;
     });
+    res.render("profile", profile);
+});
 
-    router.get("/profiles/:id", (req, res) => {
-<<<<<<< HEAD
-        const viewData = {
-            data: data
-        }
-=======
-  const viewData = {
-    data: data
-  };
-  //console.log(viewData.data);
->>>>>>> 34ff2ffcac38b0b617a6c99b8c63c161d94b5629
-
-        const profile = viewData.data.profiles.find(item => {
-            return item.id == req.params.id;
-        });
-        res.render("profile", profile);
+router.get("/profiles/edit/:id", (req, res) => {
+    const profile = data.profiles.find(function (item) {
+        return item.id == req.params.id;
     });
+    res.render("edit", profile);
+});
 
-    router.get("/profiles/edit/:id", (req, res) => {
-        const profile = data.profiles.find(function (item) {
-            return item.id == req.params.id;
-        });
-        res.render("edit", profile);
-    });
-
-<<<<<<< HEAD
-    router.post('/profiles/edit/:id', (req, res) => {
-        console.log(req.body)
-=======
 router.post("/profile/edit/:id", (req, res) => {
-  let update = req.body; //form data
-  console.log("REQ BODY: ", update);
->>>>>>> 34ff2ffcac38b0b617a6c99b8c63c161d94b5629
+    let update = req.body; //form data
+    console.log("REQ BODY: ", update);
 
-        let profileToUpdate = findInArray(req.params.id);
-        console.log("PROFILE TO UPDATE: ", profileToUpdate);
+    let profileToUpdate = findInArray(req.params.id);
+    console.log("PROFILE TO UPDATE: ", profileToUpdate);
 
-<<<<<<< HEAD
-        // const profile = viewData.data.profiles.find(item => {
-        //     return item.id == req.params.id;
-        // });
+    profileToUpdate.name = update.name;
+    profileToUpdate.blog = update.blog;
+    profileToUpdate.bio = update.bio;
 
-
-        // res.render('profile', profile);
-
-    })
-
-
-    // router.post("/edit/:id", (req, res) => {
-    //     const updateProfile = req.body
-    //     const profile = data.profiles.find(function (item) {
-    //         return item.id == req.params.id;
-    //     })
-
-    //     Object.assign(profile, updateProfile);
-
-    //     fs.writeFile("data.json", JSON.stringify(data, null, 2), "utf8", err => {
-    //         if (err) throw err;
-    //         console.log("The profile has been edited");
-    //     });
-    //     console.log(updateProfile)
-
-    // });
-=======
-  profileToUpdate.name = update.name;
-  profileToUpdate.blog = update.blog;
-  profileToUpdate.bio = update.bio;
-
-  //Write the entire array back into the JSON file
-  writeUpdateToFile(data, () => res.redirect("/profiles/" + req.params.id)); //Redirect goes to urls render to views
+    //Write the entire array back into the JSON file
+    writeUpdateToFile(data, () => res.redirect("/profiles/" + req.params.id)); //Redirect goes to urls render to views
 });
 
 function findInArray(id) {
-  //locate the puppy we are going to update
-  return data.profiles.find(item => {
-    return item.id == id;
-  });
+    //locate the puppy we are going to update
+    return data.profiles.find(item => {
+        return item.id == id;
+    });
 }
 
 function writeUpdateToFile(updateArray, callback) {
-  fs.writeFile(
-    "data.json",
-    JSON.stringify(updateArray, null, 2),
-    "utf8",
-    err => {
-      if (err) throw err;
-      console.log("The profile has been updated");
-      callback();
-    }
-  );
+    fs.writeFile(
+        "data.json",
+        JSON.stringify(updateArray, null, 2),
+        "utf8",
+        err => {
+            if (err) throw err;
+            console.log("The profile has been updated");
+            callback();
+        }
+    );
 }
->>>>>>> 34ff2ffcac38b0b617a6c99b8c63c161d94b5629
 
-    module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,21 +2,6 @@ const express = require("express");
 const hbs = require("express-handlebars");
 const routes = require("./routes");
 
-<<<<<<< HEAD
-const app = express()
-
-// Middleware
-app.engine('hbs', hbs({
-  defaultLayout: 'main',
-  extname: 'hbs'
-}))
-
-app.set('view engine', 'hbs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: false }))
-//*************************************************************************** */
-app.use('/', routes)
-=======
 const app = express();
 
 // Middleware
@@ -35,6 +20,5 @@ app.use("/", routes);
 app.get("/", (req, res) => {
   res.send("Server is working");
 });
->>>>>>> 34ff2ffcac38b0b617a6c99b8c63c161d94b5629
 
 module.exports = app;
